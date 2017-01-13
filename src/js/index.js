7e0f676c5ec1bfd62c1ed95a7040993ef71e91cb
Sylphony <symphonictwist@gmail.com>
2017-01-12 21:53:18 -0800

    Added makeRequest(); placed a test run

---
+++ src/js/index.js
@@ -0,0 +1,42 @@
+/**
+ * makeRequest()
+ * Makes an asynchronous request.
+ */
+function makeRequest(method, url) {
+    return new Promise(function(resolve, reject) {
+        var request = new XMLHttpRequest();
+
+        request.onload = function() {
+            resolve(request);
+        };
+
+        request.onerror = function() {
+            reject(request);
+        };
+
+        request.open(method, url);
+        request.send();
+    });
+}
+
+makeRequest("GET", "http://jsonplaceholder.typicode.com/photos")
+    .then(function(response) {
+        var data = JSON.parse(response.response);
+
+        var ulTag = "<ul>";
+        var liTags = "";
+
+        for (var index = 0; index < 20; index++) {
+            liTags = liTags
+                    +   "<li class=\"listview__list-item\">" 
+                    +   "<img class=\"listview__list-item-image\" src=\"" + data[index].thumbnailUrl + "\">"
+                    +   "<span class=\"listview__list-item-text\">" + index + " - " + data[index].title + "</span>"
+                    +   "</li>";
+        }
+
+        ulTag = ulTag + liTags + "</ul>";
+
+        document.getElementById("listview").innerHTML = ulTag;   
+    }, function(response) {
+        console.log("failure");
+    });
