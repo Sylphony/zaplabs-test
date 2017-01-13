9fda77b45864324577f33ee8a3e429e5db06794e
Sylphony <symphonictwist@gmail.com>
2017-01-13 00:05:42 -0800

    li elements are appended to the DOM with a fragment now

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,9 @@
 /**
  * makeRequest()
  * Makes an asynchronous request.
+ * @param method: The method.
+ * @param url: The url.
+ * @return : A promise.
  */
 function makeRequest(method, url) {
     return new Promise(function(resolve, reject) {
@@ -23,20 +26,23 @@ makeRequest("GET", "http://jsonplaceholder.typicode.com/photos")
     .then(function(response) {
         var data = JSON.parse(response.response);
 
-        var ulTag = "<ul class=\"listview__list\">";
-        var liTags = "";
+        // Create a fragment which acts like a psuedo-DOM node to hold the child elements of the list
+        var fragment = document.createDocumentFragment();
 
         for (var index = 0; index < 20; index++) {
-            liTags = liTags
-                    +   "<li class=\"listview__list-item clearfix\">" 
-                    +   "<img class=\"listview__list-item-image\" src=\"" + data[index].thumbnailUrl + "\">"
-                    +   "<span class=\"listview__list-item-title\">" + index + " - " + data[index].title + "</span>"
-                    +   "</li>";
+            var ele = document.createElement("li");
+            ele.className = "listview__list-item clearfix";
+            ele.innerHTML = 
+                    "<img class=\"listview__list-item-image\" src=\"" + data[index].thumbnailUrl + "\">"
+                +   "<span class=\"listview__list-item-title\">" + index + " - " + data[index].title + "</span>"
+                +   "</li>";
+
+            fragment.appendChild(ele);
         }
 
-        ulTag = ulTag + liTags + "</ul>";
+        // Append the fragment to the DOM
+        document.querySelector(".listview__list").appendChild(fragment);
 
-        document.getElementById("listview").innerHTML = ulTag;   
     }, function(response) {
         console.log("failure");
     });
